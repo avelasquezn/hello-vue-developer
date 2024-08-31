568b733ef13979f99a9336c81b86fa1c57a30b6a
Angel Velasquez <pcisavel@upc.edu.pe>
2024-08-31 12:00:33 -0500

    feat(welcoming): added register developer component.

---
+++ src/welcoming/components/register-developer.component.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "register-developer",
+  data() {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log("Registering developer");
+      this.$emit( 'developer-registered', { firstName: this.firstName, lastName: this.lastName});
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
